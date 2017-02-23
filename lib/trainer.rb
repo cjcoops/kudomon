76fbea865812640764ccaf129250d029e0ac6ad2
Chris Cooper <cjcooper252@gmail.com>
2017-02-23 11:13:06 +0000

    initializes trainer with grid position

---
+++ lib/trainer.rb
@@ -0,0 +1,8 @@
+class Trainer
+
+  attr_reader :position
+
+  def initialize(position: position)
+    @position = position
+  end
+end
---
+++ spec/trainer_spec.rb
@@ -0,0 +1,10 @@
+require 'trainer'
+
+describe Trainer do
+
+  subject(:trainer) {described_class.new(position: [1,2])}
+
+  it "is initialized with a 2d grid position" do
+    expect(subject.position).to eq([1,2])
+  end
+end
